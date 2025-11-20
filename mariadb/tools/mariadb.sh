2065be013ff7bcf6318a38878227edb7468fc858
thaismeneses <thaismeneses@Inception.myguest.virtualbox.org>
2025-11-20 20:16:51 -0300

    add: docker-compose file and reconfig Dockerfile from mariadb

--- mariadb/tools/mariadb.sh
+++ mariadb/tools/mariadb.sh
@@ -1,17 +1,45 @@
 #!/bin/bash
+set -e
 
-service mariadb start
+mkdir -p /run/mysqld
+chown -R mysql:mysql /run/mysqld
 
-sleep 5
+chown -R mysql:mysql /var/lib/mysql
 
-mysql -e "CREATE DATA IF NOT EXISTS ${MYSQL_DATABASE};"
+# Inicializa o banco caso ainda não exista
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "Inicializando o diretório do banco..."
+    mysql_install_db --user=mysql --datadir=/var/lib/mysql > /dev/null
+fi
 
-mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+echo "Iniciando MariaDB temporário..."
+/usr/sbin/mysqld --user=mysql --datadir=/var/lib/mysql --skip-networking &
+pid="$!"
 
-mysql -e "GRANT ALL PRIVILAGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+echo "Aguardando MariaDB Iniciar..."
+until mysqladmin --protocol=socket ping --silent; do
+    sleep 1
+done
 
-mysql -e "FLUSH PRIVILEGES;"
 
-mysqladmin shutdown 
+echo "Configurando banco e usuários..."
 
-exec mysqld_safe
+mysql --protocol=socket -u root << EOF
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+
+CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+
+CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+
+GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+
+FLUSH PRIVILEGES;
+EOF
+
+
+echo "Parando instância temporária..."
+kill "$pid"
+sleep 2
+
+echo "Iniciando MariaDB definitivo..."
+exec /usr/sbin/mysqld --user=mysql --datadir=/var/lib/mysql
